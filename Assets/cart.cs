7a287e05eb68361c8fae315150efb7b97b11cbaa
CNB16 <150976200+CNBburnaby@users.noreply.github.com>
2024-01-10 17:08:51 -0800

    Added Main Menu

--- Assets/cart.cs
+++ Assets/cart.cs
@@ -24,6 +24,7 @@ public class cart : MonoBehaviour
         {
             horizontal = Input.GetAxisRaw("Horizontal1");
             vertical = Input.GetAxisRaw("Vertical1");
+            Debug.Log(horizontal);
         }
         else
         {
